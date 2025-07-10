cdc4e64989be4e40b82828812b8decce7c3e6634
Chaitanya <ch4174nya@gmail.com>
2025-07-10 19:41:16 +0800

    replace dotenv with st.secrets
    this is for deployment on streamlit cloud

--- feedback_engine.py
+++ feedback_engine.py
@@ -1,10 +1,13 @@
 import openai
+import streamlit as st
+
+client = openai.Client(api_key=st.secrets["OPENAI_API_KEY"])
 
 def rewrite_resume_line(line):
     prompt = f"""
     You are a resume optimization assistant. Rewrite this bullet point to make it clearer, more impactful and quantifiable if possible.
     """
-    response = openai.responses.create(
+    response = client.responses.create(
         model="gpt-4o-mini",
         instructions=prompt,
         input=line
@@ -17,7 +20,7 @@ def suggest_resume_improvements(resume_text):
     1. Missing key skills or technologies
     2. Revisions to make statements more impactful and quantifiable if possible
     """
-    response = openai.responses.create(
+    response = client.responses.create(
         model="gpt-4o-mini",
         instructions=prompt,
         input=resume_text
--- role_matcher.py
+++ role_matcher.py
@@ -1,7 +1,7 @@
 import openai
-from dotenv import load_dotenv
-load_dotenv()
+import streamlit as st
 
+client = openai.Client(api_key=st.secrets["OPENAI_API_KEY"])
 
 def suggest_roles(resume_text):
     prompt = f"""
@@ -10,7 +10,7 @@ def suggest_roles(resume_text):
     - Role 1: <title> - <1-line reason>
     - Role 2: ...
     """
-    response = openai.responses.create(
+    response = client.responses.create(
         model="gpt-4o-mini",
         instructions=prompt,
         input=resume_text
